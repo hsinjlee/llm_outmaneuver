6fd4da585c4b0250069d0e125c69d9d3f909feb5
Robert Lee <rlee4408@gmail.com>
2024-12-05 22:43:00 +0800

    Add all modules

---
+++ app.py
@@ -0,0 +1,44 @@
+"""
+Entry point for the Outmaneuver Arena LLM Battle
+Initialize logging, env variables and styling as needed
+Check if an Arena is in the session, and if not, create a new one using Arena.default()
+Delegate to a Display object to manage the drawing of the UI components
+
+To see it in action, run:
+python -m streamlit run app.py
+"""
+
+from dotenv import load_dotenv
+import logging
+from game.arenas import Arena
+import streamlit as st
+from util.setup import setup_logger, STYLE
+from views.displays import Display
+
+root = logging.getLogger()
+if "root" not in st.session_state:
+    st.session_state.root = root
+    setup_logger(root)
+
+load_dotenv()
+
+st.set_page_config(
+    layout="wide",
+    page_title="Outmaneuver",
+    menu_items={
+        "About": "outmaneuver is an LLM arena that pits LLMs against each other in a battle of negotiation."
+    },
+    page_icon="🧠",
+    initial_sidebar_state="collapsed",
+)
+st.markdown(STYLE, unsafe_allow_html=True)
+
+if "arena" not in st.session_state:
+    st.session_state.arena = Arena.default()
+
+if "auto_move" not in st.session_state:
+    st.session_state.auto_move = False
+    st.session_state.do_move = False
+arena = st.session_state.arena
+
+Display(arena).display_page()
---
+++ views/sidebars.py
@@ -0,0 +1,66 @@
+import os
+import streamlit as st
+from game.arenas import Arena
+from models.games import Game
+
+
+def display_ranks():
+    st.markdown(
+        "<span style='font-size:13px;'>The table is sorted initially by Win %. "
+        "The skill ratings use the TrueSkill methodology,"
+        " an ELO-style system for multi-player games.</span>",
+        unsafe_allow_html=True,
+    )
+    column_config = {
+        "LLM": st.column_config.TextColumn(width="small"),
+        "Win %": st.column_config.NumberColumn(format="%.1f"),
+        "Skill": st.column_config.NumberColumn(format="%.1f"),
+    }
+    st.dataframe(data=Arena.rankings(), hide_index=True, column_config=column_config)
+
+
+def display_latest():
+    st.write("Latest games")
+    column_config = {
+        "When": st.column_config.DatetimeColumn(width="small"),
+        "Winner(s)": st.column_config.TextColumn(width="medium"),
+    }
+    st.dataframe(data=Arena.latest(), hide_index=True, column_config=column_config)
+
+
+def display_coffee():
+    st.markdown(
+        "<span style='font-size:13px;'>If you enjoyed Outmaneuver, I'm super grateful for any recognition, but not required at all!</span>",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        """
+        <div style="display: flex; justify-content: left;">
+            <a href="https://www.buymeacoffee.com/ed.donner" target="_blank">
+                <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 40px; width: 150px;">
+            </a>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+def display_sidebar():
+    with st.sidebar:
+        st.markdown("### Outmaneuver Leaderboard")
+        if os.getenv("MONGO_URI"):
+            try:
+                st.write(f"There have been {Game.count():,} games recorded.")
+                if st.button("Calculate Rankings"):
+                    display_ranks()
+                    display_latest()
+            except Exception as e:
+                st.write(
+                    "Unable to calculate rankings - the database may not be available."
+                )
+                st.write(f"Underlying error was {e}")
+        else:
+            st.write(
+                "LLM rankings aren't available as this app isn't connected to the database"
+            )
+        #display_coffee()
